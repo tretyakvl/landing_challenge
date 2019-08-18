57b30c27fc6bdaa78f02b8e354055a1bd0148485
Sane <tretyakvl@gmail.com>
2019-08-18 20:16:46 +0300

    Adds browserify to server task

--- gruntfile.js
+++ gruntfile.js
@@ -63,5 +63,5 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('server', ['sass', 'browserSync', 'watch']);
+  grunt.registerTask('server', ['sass', 'browserify', 'browserSync', 'watch']);
 };
