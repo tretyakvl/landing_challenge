d60278bc97f376d3184c32b30c1d00cf3459bab5
Sane <tretyakvl@gmail.com>
2019-08-21 14:50:09 +0300

    Adds scroll lock when mobile nav is opened

--- source/js/modules/header.js
+++ source/js/modules/header.js
@@ -1,6 +1,8 @@
-const header = document.querySelector('.header')
+const body = document.querySelector('body')
+const header = body.querySelector('.header')
 const button = header.querySelector('.header__button')
 
 button.addEventListener('click', () => {
+  body.classList.toggle('no-scroll')
   header.classList.toggle('header--opened')
 })
